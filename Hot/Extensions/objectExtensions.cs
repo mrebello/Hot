5a23f753b5225b1c9fb92d21114695bc4c56970a
Marcos Paulo Ferreira Rebello <rebello@mensa.org.br>
2024-07-30 17:51:56 -0300

    Conversões dos tipos com DBNULL

--- Hot/Extensions/objectExtensions.cs
+++ Hot/Extensions/objectExtensions.cs
@@ -38,8 +38,7 @@ namespace Hot.Extensions {
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public static decimal? ToDecimal(this object v)
-        {
+        public static decimal? ToDecimal(this object v) {
             if (v is null)
                 return null;
             if (v is DBNull)
@@ -47,6 +46,139 @@ namespace Hot.Extensions {
             return (decimal)v;
         }
 
+        /// <summary>
+        /// Retorna objeto convertido para float, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static float? ToFloat(this object v) {
+            if (v is null)
+                return null;
+            if (v is DBNull)
+                return null;
+            return (float)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para long, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static long? ToLong(this object v) {
+            if (v == null || v is DBNull)
+                return null;
+            return (long)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para double, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static double? ToDouble(this object v) {
+            if (v == null || v is DBNull)
+                return null;
+            return (double)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para bool, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static bool? ToBool(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (bool)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para DateTime, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static DateTime? ToDateTime(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (DateTime)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para char, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static char? ToChar(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (char)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para byte, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static byte? ToByte(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (byte)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para short, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static short? ToShort(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (short)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para uint, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static uint? ToUInt(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (uint)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para ulong, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static ulong? ToULong(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (ulong)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para ushort, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static ushort? ToUShort(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (ushort)v;
+        }
+
+        /// <summary>
+        /// Retorna objeto convertido para sbyte, checando se é DBNull também. Caso seja null ou DBNull, retorna null;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static sbyte? ToSByte(this object v) {
+            if (v is null || v is DBNull)
+                return null;
+            return (sbyte)v;
+        }
         /// <summary>
         /// Transforma o objeto em uma string para visualização, listando todas as propriedades. (Usar para depuração)
         /// </summary>
@@ -97,10 +229,10 @@ namespace Hot.Extensions {
 
                     } else if (value is String[]) {
                         displayValue = String.Empty;
-                        foreach(var s in (value as String[])!) {
+                        foreach (var s in (value as String[])!) {
                             displayValue += String.Format("\"{0}\", ", s);
                         }
-                        if (displayValue.Length>0) displayValue = displayValue.Substring(0, displayValue.Length-2); // retira última ", "
+                        if (displayValue.Length > 0) displayValue = displayValue.Substring(0, displayValue.Length - 2); // retira última ", "
 
                     } else if (value is ICollection) {
                         var i = 0;
